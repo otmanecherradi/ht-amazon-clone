0987501d5f05ec4602d371bd42b48b38600b62d2
tommygun-hyphen <eromex02@gmail.com>
2022-05-22 12:19:01 +0100

    app header, login view, and app view

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -11,16 +11,10 @@ defineProps({
   <div class="greetings">
 
     <h1 class="text-3xl font-bold underline">
-      Hello world!
+      FakeAmazon
     </h1>
-    <h1 class="green">{{ msg }}</h1>
-
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
-  </div>
+    <h1 class="green">sell products and stuff!</h1>
+ </div>
 </template>
 
 <style scoped>
