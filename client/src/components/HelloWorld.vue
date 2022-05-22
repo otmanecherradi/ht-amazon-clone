82f4a655c2ca8b2957b364217a4ec77fc41ad5dd
Otmane <51253168+otmanecherradi@users.noreply.github.com>
2022-05-22 11:25:05 +0100

    new config

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,40 +1,49 @@
 <script setup>
-import { ref } from 'vue'
-
 defineProps({
-  msg: String
+  msg: {
+    type: String,
+    required: true
+  }
 })
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="greetings">
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <h1 class="green">{{ msg }}</h1>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+    </h3>
+  </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
 }
 </style>
